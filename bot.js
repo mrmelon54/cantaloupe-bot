54bcf0a148429a82fad3ca17ebed14b4a43854f8
MrMelon <github@onpointcoding.net>
2020-08-28 07:23:25 +0100

    kktjbrb

--- bot.js
+++ bot.js
@@ -535,8 +535,8 @@ var birthdayTimestamp = new Date(2020,7,28,7,22,0).getTime()
 function checkForBirthday() {
   if (new Date().getTime() > birthdayTimestamp && !announcedBirthday && new Date().getTime() < birthdayTimestamp + 30 * 1000) {
     announcedBirthday = true
-    client.guilds.fetch('571615112570601503').then(g=>
-      g.channels.fetch('577879389279223808').then(c=>
+    client.guilds.fetch('571615112570601503').then(g=>{
+      g.channels.fetch('577879389279223808').then(c=>{
         c.send('Hey @everyone :\n\nToday (28th August) is the birthday of <@222344019458392065> .\nWish <@222344019458392065> a happy birthday when he gets online :cake: ')
       })
     })
