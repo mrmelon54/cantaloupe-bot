2fd87337f20cab6ed3b63b6d4cba5e36599c6235
MrMelon <github@onpointcoding.net>
2021-08-28 00:01:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -564,7 +564,7 @@ function getMCServerStatus(ip, msg) {
 }
 
 var announcedBirthday = false
-var birthdayTimestamp = new Date(2020, 7, 28, 0, 0, 0).getTime()
+var birthdayTimestamp = new Date(2021, 7, 28, 0, 0, 0).getTime()
 
 function checkForBirthday() {
   if (new Date().getTime() > birthdayTimestamp && !announcedBirthday && new Date().getTime() < birthdayTimestamp + 30 * 1000) {
